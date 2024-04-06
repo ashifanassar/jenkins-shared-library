e8168653f3278c026aa81eabe4e2576f1d4e8848
ashifanassar <ashifanassar@gmail.com>
2024-04-06 05:33:45 +0530

    jenkins-shared-library

---
+++ vars/python.groovy
@@ -0,0 +1,26 @@
+def lintchecks() {
+        sh "echo Performing Lint Checks for $COMPONENT"
+        // sh "pip3 install pylint && pylint *.py"
+        sh "echo Style Checks Completed $COMPONENT"
+}
+
+def call(COMPONENT) {
+    pipeline { 
+        agent any
+        stages {
+            stage('Lint Checks') {
+                steps {
+                    script {
+                        lintchecks()
+                    }
+                }
+            }
+
+            stage('Static Code Analysis') {
+                steps {
+                    sh "echo Static Checks ...."
+                }
+            }
+        }
+    }
+}
