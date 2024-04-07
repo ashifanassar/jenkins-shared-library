cd53ea0d4a56af8381b476aed57e24d69434f8ca
ashifanassar <ashifanassar@gmail.com>
2024-04-07 07:05:50 +0530

    jenkins-shared-library

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -4,18 +4,12 @@ def lintChecks() {
     sh "node_modules/jslint/bin/jslint.js server.js || true"
 }
 
-def sonarchecks() {
-    sh '''
-        echo sonar check start on the $COMPONENT
-        sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://34.227.14.107:9000 -Dsonar.sources=. -Dsonar.login=admin -Dsonar.password=password -Dsonar.verbose=true
-        echo sonar checks starting on the $COMPONENT is completed
-    '''
-}
 
 def call(COMPONENT) {
     pipeline { 
-    agent {
-        label 'ws'
+    agent any
+    environment {
+        NEXUS_URL="34.227.14.107"
     }
     stages {
         stage('Lint Checks') {
@@ -29,7 +23,7 @@ def call(COMPONENT) {
         stage('Static Code Analysis') {
             steps {
                 script {
-                    sonarchecks()
+                    common.sonarchecks()
                 }
             }
         }
