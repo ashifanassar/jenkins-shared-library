11d4a8c84f192e37a619e6a3cf40ae4ce068675d
ashifanassar <ashifanassar@gmail.com>
2024-04-07 15:40:38 +0530

    jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,7 +1,7 @@
 def sonarchecks() {
     sh ''' 
         echo Sonar Checks Starting for $COMPONENT
-        sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.sources=. -Dsonar.login=admin -Dsonar.password=password
+        sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.host.url=http://${NEXUS_URL}:9000 ${ARGS} -Dsonar.login=admin -Dsonar.password=password
         echo Sonar Checks Starting for $COMPONENT is Completed
      '''
 }
