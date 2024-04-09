a9d0f7059ca5c5fd4f88fe8fc11a8ee240078395
ashifanassar <ashifanassar@gmail.com>
2024-04-09 17:02:36 +0530

    jenkins-shared-library

--- vars/python.groovy
+++ vars/python.groovy
@@ -8,7 +8,7 @@ def call(COMPONENT) {
     pipeline { 
     agent any
     environment {
-        NEXUS_URL="34.227.14.107"
+        NEXUS_URL="3.83.189.125"
         SONAR_CRED  = credentials('SONAR_CRED')   // SONAR_CRED_USR , SONAR_CRED_PSW
     }
     stages {
