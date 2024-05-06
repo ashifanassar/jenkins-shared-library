8203bf2e4ccd9081bbf5d422c27ca41d72cebeae
ashifanassar <ashifanassar@gmail.com>
2024-05-06 22:50:26 +0530

    modules

---
+++ vars/appinfra.groovy
@@ -0,0 +1,32 @@
+def call() {
+    properties([
+        parameters([
+            choice(name: 'ENV', choices: ['dev', 'prod'], description: 'Environment'),
+            choice(name: 'ACTION', choices: ['apply', 'destroy'], description: 'Action')                            
+        ]),
+    ])
+    node {
+        ansiColor('xterm') {
+            git branch: 'main', url: "https://github.com/b57-clouddevops/${COMPONENT}.git"
+            stage('Terraform Init') {
+                sh ''' 
+                    cd mutable-infra
+                    terrafile -f env-${ENV}/Terrafile
+                    terraform init --backend-config=env-${ENV}/${ENV}-backend.tfvars
+                ''' 
+            }
+            stage('Terraform Plan') {
+                sh ''' 
+                    cd mutable-infra
+                    terraform plan  --var-file=env-${ENV}/${ENV}.tfvars
+                ''' 
+            }
+            stage('Terraform Action') {
+                sh ''' 
+                    cd mutable-infra
+                    terraform ${ACTION} -auto-approve --var-file=env-${ENV}/${ENV}.tfvars
+                ''' 
+            }
+        }
+    }
+}
